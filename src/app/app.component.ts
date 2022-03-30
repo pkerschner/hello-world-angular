1dab3bef0bc965710cd6f31e6f0597d323e9b429
Philip Kerschner <pkerschner@zoominternet.net>
2022-03-30 17:28:39 -0400

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MaxLengthValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'The Bootcamp';
+  maxOn: boolean = true;
+  names: string[] = [ "Ann", "Bob", "Chris", "David", "Ellen" ];
+  customers: any[] = [
+    { id: 1, name: "Max", sales: 1000, state: "OH" },
+    { id: 2, name: "PG", sales: 10000, state: "OH" },
+    { id: 3, name: "Kroger", sales: 100000, state: "CA" }
+  ];
+
+  aString1: string = "";
+  aString2: string = "";
+
+  copy(): void {
+    this.aString2 = this.aString1;
+  }
+
+  clicked(): void {
+    if(this.maxOn) {
+      this.title = "The MAXimum boot camp";
+      this.maxOn = !this.maxOn;
+    } else {
+      this.maxOn = !this.maxOn;
+      this.title = "The boot camp";
+    }
+  }
 }
